887a6efb0b98709ecac70d155f10f8958979f3c3
ssh <magan0117@hotmail.com>
2015-12-03 14:46:04 +0800

    let user type letter, and print on screen
    use RXNE,TXT flag (get_char,print_char function ),and shell to implement
    you can consider the shell part as a buffer to store the char

--- 07-Threads/os.c
+++ 07-Threads/os.c
@@ -7,86 +7,83 @@
  * This flag is cleared when data is written to USARTx_DR and
  * set when that data is transferred to the TDR
  */
-#define USART_FLAG_TXE	((uint16_t) 0x0080)
+#define USART_FLAG_TXE        ((uint16_t) 0x0080)
+#define USART_FLAG_RXNE        ((uint16_t) 0x0020)
 /*fub function*/
-    int fib(int num)
-    {
-            if (num == 0)
-                    return 0;
+int fib(int num)
+{
+    if (num == 0)
+        return 0;
 
-            int res;
-            asm volatile("push {r3, r4, r5, r6}");
-            asm volatile("mov r6, %[arg]" :: [arg] "r"(num) :);
-            asm volatile("mov r3,#0\n"
-                         "mov r4,#1\n"
-                         "mov r5,#0\n"
-                         ".forloop:\n"
-                         "mov r3,r4\n"
-                         "mov r4,r5\n"
-                         "add r5,r3,r4\n"
-                         "subs r6,r6,#1\n"
-                         "bgt .forloop\n");
-            asm volatile("mov %[result], r5" : [result] "=r"(res) ::);
-            asm volatile("pop {r3, r4, r5, r6}");
-            return res;
-    }
+    int res;
+    asm volatile("push {r3, r4, r5, r6}");
+    asm volatile("mov r6, %[arg]" :: [arg] "r"(num) :);
+    asm volatile("mov r3,#0\n"
+                 "mov r4,#1\n"
+                 "mov r5,#0\n"
+                 ".forloop:\n"
+                 "mov r3,r4\n"
+                 "mov r4,r5\n"
+                 "add r5,r3,r4\n"
+                 "subs r6,r6,#1\n"
+                 "bgt .forloop\n");
+    asm volatile("mov %[result], r5" : [result] "=r"(res) ::);
+    asm volatile("pop {r3, r4, r5, r6}");
+    return res;
+}
 void usart_init(void)
 {
-	*(RCC_APB2ENR) |= (uint32_t) (0x00000001 | 0x00000004);
-	*(RCC_APB1ENR) |= (uint32_t) (0x00020000);
+    *(RCC_APB2ENR) |= (uint32_t) (0x00000001 | 0x00000004);
+    *(RCC_APB1ENR) |= (uint32_t) (0x00020000);
 
-	/* USART2 Configuration, Rx->PA3, Tx->PA2 */
-	*(GPIOA_CRL) = 0x00004B00;
-	*(GPIOA_CRH) = 0x44444444;
-	*(GPIOA_ODR) = 0x00000000;
-	*(GPIOA_BSRR) = 0x00000000;
-	*(GPIOA_BRR) = 0x00000000;
+    /* USART2 Configuration, Rx->PA3, Tx->PA2 */
+    *(GPIOA_CRL) = 0x00004B00;
+    *(GPIOA_CRH) = 0x44444444;
+    *(GPIOA_ODR) = 0x00000000;
+    *(GPIOA_BSRR) = 0x00000000;
+    *(GPIOA_BRR) = 0x00000000;
 
-	*(USART2_CR1) = 0x0000000C;
-	*(USART2_CR2) = 0x00000000;
-	*(USART2_CR3) = 0x00000000;
-	*(USART2_CR1) |= 0x2000;
+    *(USART2_CR1) = 0x0000000C;
+    *(USART2_CR2) = 0x00000000;
+    *(USART2_CR3) = 0x00000000;
+    *(USART2_CR1) |= 0x2000;
 }
 
 void print_str(const char *str)
 {
-	while (*str) {
-		while (!(*(USART2_SR) & USART_FLAG_TXE));
-		*(USART2_DR) = (*str & 0xFF);
-		str++;
-	}
+    while (*str) {
+        while (!(*(USART2_SR) & USART_FLAG_TXE));
+        *(USART2_DR) = (*str & 0xFF);
+        str++;
+    }
 }
-
-static void delay(volatile int count)
+void print_char(const char *chr)
 {
-	count *= 50000;
-	while (count--);
+    if (*chr) {
+        while (!(*(USART2_SR) & USART_FLAG_TXE));
+        *(USART2_DR) = (*chr & 0xFF);
+    }
 }
 
-static void busy_loop(void *str)
+char get_char(void)
 {
-	while (1) {
-		print_str(str);
-		print_str(": Running...\n");
-		delay(1000);
-	}
+    while (!(*(USART2_SR) & USART_FLAG_RXNE));
+    return *(USART2_DR) & 0xFF;
 }
 
-void test1(void *userdata)
+
+void shell(void *userdata)
 {
-	busy_loop(userdata);
+    char buf[30];
+    int i;
+    while(1) {
+        print_str((char *) userdata);
+        for(i=0; i<30; i++) {
+            buf[i] = get_char();
+            print_char(&buf[i]);
+        }
+    }
 }
-
-void test2(void *userdata)
-{
-	busy_loop(userdata);
-}
-
-void test3(void *userdata)
-{
-	busy_loop(userdata);
-}
-
 /* 72MHz */
 #define CPU_CLOCK_HZ 72000000
 
@@ -95,25 +92,17 @@ void test3(void *userdata)
 
 int main(void)
 {
-	const char *str1 = "Task1", *str2 = "Task2", *str3 = "Task3";
+    const char *str4 = "$ ";
+    usart_init();
+    if(thread_create(shell, (void *) str4) == -1)
+        print_str("Thread 4 creation failed\r\n");
 
-	usart_init();
+    /* SysTick configuration */
+    *SYSTICK_LOAD = (CPU_CLOCK_HZ / TICK_RATE_HZ) - 1UL;
+    *SYSTICK_VAL = 0;
+    *SYSTICK_CTRL = 0x07;
 
-	if (thread_create(test1, (void *) str1) == -1)
-		print_str("Thread 1 creation failed\r\n");
+    thread_start();
 
-	if (thread_create(test2, (void *) str2) == -1)
-		print_str("Thread 2 creation failed\r\n");
-
-	if (thread_create(test3, (void *) str3) == -1)
-		print_str("Thread 3 creation failed\r\n");
-
-	/* SysTick configuration */
-	*SYSTICK_LOAD = (CPU_CLOCK_HZ / TICK_RATE_HZ) - 1UL;
-	*SYSTICK_VAL = 0;
-	*SYSTICK_CTRL = 0x07;
-
-	thread_start();
-
-	return 0;
+    return 0;
 }
